c3532eae03f691aab01ac30940106d6c2776d889
Madophs <jehuruv@hotmail.com>
2019-05-06 19:36:19 -0500

    Bugs fixed, optimize performance

--- BigInteger.cpp
+++ BigInteger.cpp
@@ -43,9 +43,11 @@ class BigInteger{
 		void removeLeadingZeros();
 		void makePositiveIfZero();
 		unsigned int & getValueAt(int);
-		BigInteger addition(BigInteger &,BigInteger &);
+		BigInteger addition(BigInteger ,BigInteger );
 		BigInteger substraction(BigInteger, BigInteger &);
 		BigInteger multiplication(BigInteger, BigInteger);
+		BigInteger longMultiplication(BigInteger &, BigInteger &);
+		BigInteger karatsubaMultiplication(BigInteger&, BigInteger&);
 		BigInteger division(BigInteger &, BigInteger &);
 		BigInteger pow(BigInteger &);
 		BigInteger pow(int);
@@ -132,7 +134,7 @@ bool BigInteger::operator == (BigInteger &bigNumber){
 	if(this->positive != bigNumber.positive) return false;
 	if(value.size() != bigNumber.length()) return false;
 	for(int i = 0; i<bigNumber.length(); i++){
-		if(getValueAt(i) != bigNumber.getValueAt(i)) return false;
+		if(value[i] != bigNumber.value[i]) return false;
 	}
 	return true;
 }
@@ -141,19 +143,19 @@ bool BigInteger::operator != (BigInteger &bigNumber){
 	if(this->positive != bigNumber.positive) return true;
 	if(value.size() != bigNumber.length()) return true;
 	for(int i = 0; i<bigNumber.length(); i++){
-		if(getValueAt(i) != bigNumber.getValueAt(i)) return true;
+		if(value[i] != bigNumber.value[i]) return true;
 	}
 	return false;
 }
 bool BigInteger::operator < (BigInteger &bigNumber){
 	if(!this->positive && !bigNumber.positive){
-		if(this->length() != bigNumber.length()){
-			if(this->length() > bigNumber.length()) return true;
+		if(this->value.size() != bigNumber.value.size()){
+			if(this->value.size() > bigNumber.value.size()) return true;
 			return false;
 		}
 		for(int i = this->length()-1; i>=0; i--){
-			if(this->getValueAt(i) == bigNumber.getValueAt(i)) continue;
-			if(this->getValueAt(i) > bigNumber.getValueAt(i)) return true;
+			if(this->value[i] == bigNumber.value[i]) continue;
+			if(this->value[i] > bigNumber.value[i]) return true;
 		}
 		return false;
 	}
@@ -161,8 +163,8 @@ bool BigInteger::operator < (BigInteger &bigNumber){
 		if(!this->positive && bigNumber.positive) return true;
 		return false;
 	}
-	if(this->length() != bigNumber.length()){
-		if(this->length() < bigNumber.length()) return true;
+	if(this->value.size() != bigNumber.value.size()){
+		if(this->value.size() < bigNumber.value.size()) return true;
 		return false;
 	}
 	for(int i = this->length()-1; i>=0; i--){
@@ -174,14 +176,14 @@ bool BigInteger::operator < (BigInteger &bigNumber){
 
 bool BigInteger::operator <= (BigInteger &bigNumber){
 	if(!this->positive && !bigNumber.positive){
-		if(this->length() != bigNumber.length()){
-			if(this->length() > bigNumber.length()) return true;
+		if(this->value.size() != bigNumber.value.size()){
+			if(this->value.size() > bigNumber.value.size()) return true;
 			return false;
 		}
 		for(int i = this->length()-1; i>=0; i--){
-			if(this->getValueAt(i) > bigNumber.getValueAt(i)) return true;
-			if(this->getValueAt(i) == bigNumber.getValueAt(i)) continue;
-			if(this->getValueAt(i) < bigNumber.getValueAt(i)) return false;
+			if(this->value[i] > bigNumber.value[i]) return true;
+			if(this->value[i] == bigNumber.value[i]) continue;
+			if(this->value[i] < bigNumber.value[i]) return false;
 		}
 		return true;
 	}
@@ -189,11 +191,11 @@ bool BigInteger::operator <= (BigInteger &bigNumber){
 		if(!this->positive && bigNumber.positive) return true;
 		return false;
 	}
-	if(this->length() != bigNumber.length()){
-		if(this->length() < bigNumber.length()) return true;
+	if(this->value.size() != bigNumber.value.size()){
+		if(this->length() < bigNumber.value.size()) return true;
 		return false;
 	}
-	for(int i = this->length()-1; i>=0; i--){
+	for(int i = this->value.size()-1; i>=0; i--){
 		if(this->value[i] < bigNumber.value[i]) return true;
 		if(this->value[i] == bigNumber.value[i]) continue;
 		if(this->value[i] > bigNumber.value[i]) return false;
@@ -243,13 +245,13 @@ void BigInteger::readString(string str){
 string BigInteger::toString(){
 	string ret = "";
 	for(int i = this->length()-1; i>=0; i--){
-		ret += this->getValueAt(i)+'0';
+		ret += this->value[i]+'0';
 	}
 	if(!isPositive()) ret = "-" + ret;
 	return ret;
 }
 void BigInteger::addLeadingZeros(int zeros){
-	while(zeros--) value.emplace_back(0);
+	value.resize(value.size()+zeros);
 }
 
 void BigInteger::removeLeadingZeros(){
@@ -257,109 +259,134 @@ void BigInteger::removeLeadingZeros(){
 		value.pop_back();
 }
 void BigInteger::makePositiveIfZero(){
-	if(this->length() > 1) return;
+	if(this->value.size()> 1) return;
 	if(!this->getValueAt(0)) positive = true;
 }
 
 void BigInteger::addToTheBeginning(int val, int cant){
-	while(cant--) value.insert(value.begin(),val);
+	if(cant<=0) return;
+	value.resize(value.size()+cant,val);
+	for(int i = value.size()-1; i>=cant; i--){
+		swap(value[i],value[i-cant]);
+	}
 }
 
 unsigned int & BigInteger::getValueAt(int index){
 	return value[index];
 }
-BigInteger BigInteger::addition(BigInteger &big1, BigInteger &big2){
+BigInteger BigInteger::addition(BigInteger big1, BigInteger big2){
 	if(big1.isPositive() != big2.isPositive()){
-		if(big1.length() != big2.length()){
-			if(big1.length() > big2.length()){
+		if(big1.value.size() != big2.value.size()){
+			if(big1.value.size() > big2.value.size()){
 				return substraction(big1,big2);
 			}else{
 				return substraction(big2,big1);
 			}
 		}else{
-			for(int i = big1.length()-1; i>=0; i--){
-				if(big1.getValueAt(i) == big2.getValueAt(i)) continue;
-				if(big1.getValueAt(i) > big2.getValueAt(i)) return substraction(big1,big2);
+			for(int i = big1.value.size()-1; i>=0; i--){
+				if(big1.value[i] == big2.value[i]) continue;
+				if(big1.value[i] > big2.value[i]) return substraction(big1,big2);
 				return substraction(big2,big1);
 			}
 			return BigInteger();
 		}
 	}
 
-	BigInteger result;
-	result.addLeadingZeros(max(big1.length(),big2.length())+1);
-	int i = 0, carry = 0, maxLength = max(big1.length(), big2.length());
-	for(; i < maxLength; i++){
-		int sum = (i<big1.length()?big1.getValueAt(i):0) + (i<big2.length()?big2.getValueAt(i):0) + carry;
+	if(big1.value.size() < big2.value.size()) swap(big1,big2);
+	int i = 0, carry = 0, sum;
+	big1.addLeadingZeros(1);
+	for(; i < big1.length(); i++){
+		sum = big1.value[i] + big2.value[i] + carry;
 		carry = sum/10;
-		result.getValueAt(i) = sum%10;
+		big1.value[i] = sum%10;
+		if(i+1 == big2.value.size() && ++i) break;
 	}
-	while(carry) result.getValueAt(i++) = carry, carry = carry/10;
-	result.removeLeadingZeros();
-	result.positive = big1.positive;
-	result.makePositiveIfZero();
-	return result;
+	while(carry)
+		sum = big1.value[i] + carry,
+		big1.value[i++] = sum%10, carry = sum/10;
+
+	big1.removeLeadingZeros();
+	big1.makePositiveIfZero();
+	return big1;
 }
 
 BigInteger BigInteger::substraction(BigInteger big1, BigInteger &big2){
-	BigInteger result;
-	result.addLeadingZeros(big1.length());
-	for(int i = 0; i<big1.length(); i++){
-		int diff = 0;
-		if(i < big2.length() && big1.getValueAt(i) < big2.getValueAt(i)){
-			for(int j = i+1; j<big1.length(); j++){
-				if(big1.getValueAt(j)){
-					--big1.getValueAt(j);
+	big1.addLeadingZeros(1);
+	for(int i = 0; i<big1.value.size(); i++){
+		if(big1.value[i] < big2.value[i]){
+			for(int j = i+1; j<big1.value.size(); j++){
+				if(big1.value[j]){
+					--big1.value[j];
 					break;
 				}else{
-					big1.getValueAt(j) = 9;
+					big1.value[j] = 9;
 				}
 			}
-			int val1 = big1.getValueAt(i)+10, val2 = big2.getValueAt(i);
-			result.getValueAt(i) = val1-val2;
+			int val1 = big1.value[i]+10, val2 = big2.value[i];
+			big1.value[i] = val1-val2;
+			if(i+1==big2.value.size()) break;
 		}else{
-			result.getValueAt(i) = big1.getValueAt(i) - (i<big2.length()?big2.getValueAt(i):0);
+			big1.value[i] = big1.value[i] - big2.value[i];
+			if(i+1==big2.value.size()) break;
 		}
 	}
-	if(!big1.isPositive()){
-		result.positive = false;
-	}
-	result.removeLeadingZeros();
-	result.makePositiveIfZero();
-	return result;
+	big1.removeLeadingZeros();
+	big1.makePositiveIfZero();
+	return big1;
 }
 
 BigInteger BigInteger::multiplication(BigInteger big1, BigInteger big2){
-	if(big1.length() == 1 && big2.length() == 1){
-		int val1 = atoi(big1.toString().c_str()), val2 = atoi(big2.toString().c_str());;
-		BigInteger result(to_string(val1*val2));
-		return result;
-	}
-	if(big1.length() & 1) big1.addLeadingZeros(1);
-	if(big2.length() & 1) big2.addLeadingZeros(1);
-	int maxLength = max(big1.length(), big2.length());
-	big1.addLeadingZeros(maxLength-big1.length());
-	big2.addLeadingZeros(maxLength-big2.length());
-	BigInteger bigNum1H, bigNum1L, bigNum2H, bigNum2L;
-	int splitSize = maxLength>>1;
-	bigNum1H.addLeadingZeros(splitSize-1), bigNum1L.addLeadingZeros(splitSize-1),
-	bigNum2H.addLeadingZeros(splitSize-1), bigNum2L.addLeadingZeros(splitSize-1);
-	for(int i = 0; i<maxLength; i++){
-		if(i < splitSize){
-			bigNum1H[i] = big1[i];
-			bigNum2H[i] = big2[i]; 
-		}else{
-			bigNum1L[i-splitSize] = big1[i];
-			bigNum2L[i-splitSize] = big2[i];
+	return karatsubaMultiplication(big1, big2);
+}
+
+BigInteger BigInteger::longMultiplication(BigInteger &big1, BigInteger &big2){
+	BigInteger result;
+	result.value.resize(big1.value.size()+big2.value.size()+1);
+	int i,j,carry = 0, product;
+	for(i = 0; i<big1.value.size(); i++){
+		carry = 0;
+		for(j = 0; j<big2.value.size(); j++){
+			product = (big2.value[j]*big1.value[i]) + carry + result.value[i+j];
+			carry = product/10;
+			result.value[i+j] = product%10;
 		}
+		while(carry)
+			product = result.value[i+j] + carry,
+			result.value[i+j] = product%10,
+			carry = product/10, ++j;
 	}
-	BigInteger A = multiplication(bigNum1H, bigNum2H);
-	BigInteger D = multiplication(bigNum1L, bigNum2L);
-	BigInteger E = multiplication(bigNum1H+bigNum1L,bigNum2H+bigNum2L) - A - D;
-	A.addToTheBeginning(0,maxLength);
-	E.addToTheBeginning(0,maxLength/2);
+	result.removeLeadingZeros();
+	result.positive = big1.positive == big2.positive;
+	makePositiveIfZero();
+	return result;
+}
+
+BigInteger BigInteger::karatsubaMultiplication(BigInteger &big1, BigInteger &big2){
+	if(big1.value.size() * big2.value.size() < 6000){
+		return longMultiplication(big1,big2);
+	}
+	int splitSize = (max(big1.value.size(), big2.value.size())+1)>>1;
+	BigInteger bigNum1H, bigNum1L, bigNum2H, bigNum2L;
+	if(((int)big1.value.size()) - splitSize <= 0){
+		bigNum1L.value = big1.value;
+	}else{
+		bigNum1L.value.assign(big1.value.begin(), big1.value.begin()+splitSize);
+		bigNum1H.value.assign(big1.value.begin()+splitSize,big1.value.end());
+	}
+	if(((int)big2.value.size()) - splitSize <= 0){
+		bigNum2L.value = big2.value;
+	}else{
+		bigNum2L.value.assign(big2.value.begin(), big2.value.begin()+splitSize);
+		bigNum2H.value.assign(big2.value.begin()+splitSize,big2.value.end());
+	}
+	BigInteger sumA = bigNum1H+bigNum1L, sumB = bigNum2H+bigNum2L;
+	BigInteger A = karatsubaMultiplication(bigNum1H, bigNum2H);
+	BigInteger D = karatsubaMultiplication(bigNum1L, bigNum2L);
+	BigInteger E = karatsubaMultiplication(sumA,sumB) - A - D;
+	A.addToTheBeginning(0,splitSize<<1);
+	E.addToTheBeginning(0,splitSize);
 	BigInteger result =  A + E + D;
-	if(big1.isPositive() != big2.isPositive()){
+	if(big1.positive != big2.positive){
 		result.positive = false;
 	}else{
 		result.positive = true;
@@ -368,25 +395,19 @@ BigInteger BigInteger::multiplication(BigInteger big1, BigInteger big2){
 	return result;
 }
 int main(){
+    //clock_t start, end; 
+    //start = clock();
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	BigInteger a, b, c, d, e;
-	BigInteger limit;
-	BigInteger one("1");
-	BigInteger x,y,w,z;
-	cout<<BigInteger("2314")-BigInteger("2225")-BigInteger()<<endl;
-	for(BigInteger i("00020"); i>=limit && false; i--){
-		cout<<++x<<" "<<y++<<" ";
-		cout<<--w<<" "<<z--<<endl;
-	}
-	cout<<endl;
-	cout<<"Value = "<<BigInteger("-000010")<<endl;
+	BigInteger a,b;
 	while(cin>>a>>b){
-		cout<<a<<" "<<b<<endl;
-		cout<<"Substraction = "<<a-b<<endl;
-		cout<<"Addition = "<<a+b<<endl;
-		cout<<"Multiplication = "<<a*b<<endl;
+		cout<<a*b<<"\n";
 	}
+	//end = clock(); 
+    //double time_taken = double(end - start) / double(CLOCKS_PER_SEC); 
+    //cout << "Time taken by program is : " << fixed  
+    //     << time_taken << setprecision(5); 
+    //cout << " sec " << "\n"; 
 	return 0;
 }
